c6411e35b2b3f94eb398dcc6959b3d773ca3bcfe
Bhagirathsinh8 <bhagirathnakum8@gmail.com>
2025-12-23 14:15:00 +0530

    Update Blog.jsx

--- Task3/src/componets/Blog.jsx
+++ Task3/src/componets/Blog.jsx
@@ -7,8 +7,8 @@ export default function Blog({ blogs, onDelete }) {
       {blogs.map((blog) => {
         return (
           <>
-          <div class="flex  justify-center m-4 p-2">
-            <div class="max-w-sm  rounded overflow-hidden shadow-lg">
+          <div class="flex  justify-center m-4 p-2" key={blog.id}>
+            <div class="max-w-sm  rounded overflow-hidden shadow-lg border-gray-300 border-2">
               <div class="px-6 py-4">
                 <div class="font-bold text-xl mb-2">{blog.title}</div>
                 <p class="text-gray-700 text-base mb-5">{blog.content}</p>
